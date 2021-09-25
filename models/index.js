9d6c65353f612d1b33aaa939d530b39636bd1441
Tony Phondeth <phondeth@gmail.com>
2021-09-24 20:23:50 -0400

    added associations

--- models/index.js
+++ models/index.js
@@ -3,7 +3,13 @@ const Currency = require('./Currency');
 const User = require('./User');
 
 // Create associations
+User.hasMany(Currency, {
+    foreignKey: 'user_id'
+});
 
+Currency.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 
 module.exports = {
