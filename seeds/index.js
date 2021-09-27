7badb6a26f82eab2d922f2bce127f5ad7316cca6
Tony Phondeth <phondeth@gmail.com>
2021-09-26 22:46:07 -0400

    removed duplicate code

--- seeds/index.js
+++ seeds/index.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-// const seedCategories = require('./category-seeds');
-const seedUsers = require('./user-seeds');
-// const seedTags = require('./tag-seeds');
-// const seedProductTags = require('./product-tag-seeds');
-
-const sequelize = require('../config/connection');
-
-const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedUsers();
-  console.log('\n----- Users SEEDED -----\n');
-
-  // await seedProducts();
-  // console.log('\n----- PRODUCTS SEEDED -----\n');
-
-  // await seedTags();
-  // console.log('\n----- TAGS SEEDED -----\n');
-
-  // await seedProductTags();
-  // console.log('\n----- PRODUCT TAGS SEEDED -----\n');
-
-  process.exit(0);
-};
-
-seedAll();
-=======
 const seedUsers = require('./user-seeds');
 const seedCurrencies = require('./currency-seeds');
 
@@ -48,4 +20,3 @@ const seedAll = async () => {
 };
 
 seedAll();
->>>>>>> fe251787c9948de32ec5be36fcc931204cf3ae7e
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-const { User } = require('../models');
-
-const userData = [
-  {
-    username: 'rgreen',
-    email: 'rachelgreen@mail.com',
-    password: 'password1'
-  },
-  {
-    username: 'mgeller',
-    email: 'monicageller@mail.com',
-    password: 'password2'
-  },
-  {
-    username: 'pbuffay',
-    email: 'phoebebuffay@mail.com',
-    password: 'password3'
-  },
-  {
-    username: 'jtribbiani',
-    email: 'joeytribbiani@mail.com',
-    password: 'password4'
-  },
-  {
-    username: 'cbing',
-    email: 'chandlerbing@mail.com',
-    password: 'password5'
-  },
-  {
-    username: 'rgeller',
-    email: 'rossgeller@mail.com',
-    password: 'password6'
-  }
-];
-
-const seedUsers = () => User.bulkCreate(userData);
-
-module.exports = seedUsers;
-=======
 const sequelize = require('../config/connection');
 const {
     User
@@ -76,4 +36,3 @@ const seedUsers = () => User.bulkCreate(userData, {
 });
 
 module.exports = seedUsers;
->>>>>>> fe251787c9948de32ec5be36fcc931204cf3ae7e
