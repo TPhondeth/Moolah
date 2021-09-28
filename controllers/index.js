0da8baddb6f6b6a65a09a9de25c30df434b1f959
Tony Phondeth <phondeth@gmail.com>
2021-09-28 19:06:03 -0400

    add homepage-routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/');
+const homepageRoutes = require('./homepage-routes');
+// const walletRoutes = require('./wallet-routes');
 
-
+router.use('/', homepageRoutes);
 router.use('/api', apiRoutes);
+// router.use('/wallet', walletRoutes);
 
 module.exports = router;
