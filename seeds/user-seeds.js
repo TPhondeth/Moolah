7858539aa2952bb16e7974409eb9a30df8d020fd
Sam Thabet <sallam.thabet@gmail.com>
2021-09-28 19:06:58 -0400

    testing handlebars code pushed

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,79 +1,37 @@
-<<<<<<< HEAD
-const { User } = require('../models');
+const sequelize = require("../config/connection");
+const { User } = require("../models");
 
 const userData = [
   {
-    username: 'rgreen',
-    email: 'rachelgreen@mail.com',
-    password: 'password1'
+    username: "tony",
+    email: "tony@gmail.com",
+    password: "password123",
   },
   {
-    username: 'mgeller',
-    email: 'monicageller@mail.com',
-    password: 'password2'
+    username: "mikhael",
+    email: "mikhael@gmail.com",
+    password: "password123",
   },
   {
-    username: 'pbuffay',
-    email: 'phoebebuffay@mail.com',
-    password: 'password3'
+    username: "sam",
+    email: "sam@gmail.com",
+    password: "password123",
   },
   {
-    username: 'jtribbiani',
-    email: 'joeytribbiani@mail.com',
-    password: 'password4'
+    username: "jap",
+    email: "jap@gmail.com",
+    password: "password123",
   },
   {
-    username: 'cbing',
-    email: 'chandlerbing@mail.com',
-    password: 'password5'
+    username: "mehtab",
+    email: "mehtab@gmail.com",
+    password: "password123",
   },
-  {
-    username: 'rgeller',
-    email: 'rossgeller@mail.com',
-    password: 'password6'
-  }
 ];
 
-const seedUsers = () => User.bulkCreate(userData);
+const seedUsers = () =>
+  User.bulkCreate(userData, {
+    individualHooks: true,
+  });
 
 module.exports = seedUsers;
-=======
-const sequelize = require('../config/connection');
-const {
-    User
-} = require('../models');
-
-const userData = [
-    {
-        username: 'tony',
-        email: 'tony@gmail.com',
-        password: 'password123'
-    },
-    {
-        username: 'mikhael',
-        email: 'mikhael@gmail.com',
-        password: 'password123'
-    },
-    {
-        username: 'sam',
-        email: 'sam@gmail.com',
-        password: 'password123'
-    },
-    {
-        username: 'jap',
-        email: 'jap@gmail.com',
-        password: 'password123'
-    },
-    {
-        username: 'mehtab',
-        email: 'mehtab@gmail.com',
-        password: 'password123'
-    }
-];
-
-const seedUsers = () => User.bulkCreate(userData, {
-    individualHooks: true
-});
-
-module.exports = seedUsers;
->>>>>>> fe251787c9948de32ec5be36fcc931204cf3ae7e
