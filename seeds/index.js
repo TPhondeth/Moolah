9a4da135b550b1014b429c4915523d172d170f5a
Sam Thabet <sallam.thabet@gmail.com>
2021-09-28 17:13:50 -0400

    fixed seed code

--- seeds/index.js
+++ seeds/index.js
@@ -1,59 +1,29 @@
-<<<<<<< HEAD
-// const seedCategories = require('./category-seeds');
-const seedUsers = require('./user-seeds');
-// const seedTags = require('./tag-seeds');
-// const seedProductTags = require('./product-tag-seeds');
+const seedUsers = require("./user-seeds");
+const seedCurrencies = require("./currency-seeds");
+const seedExchanges = require("./exchange-seeds");
+const seedPosts = require("./post-seeds");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
+  await sequelize.sync({
+    force: true,
+  });
+  console.log("\n----- DATABASE SYNCED -----\n");
+
   await seedUsers();
-  console.log('\n----- Users SEEDED -----\n');
+  console.log("\n----- USERS SEEDED -----\n");
 
-  // await seedProducts();
-  // console.log('\n----- PRODUCTS SEEDED -----\n');
+  await seedCurrencies();
+  console.log("\n----- CURRENCIES SEEDED -----\n");
 
-  // await seedTags();
-  // console.log('\n----- TAGS SEEDED -----\n');
+  await seedPosts();
+  console.log("\n----- POSTS SEEDED -----\n");
 
-  // await seedProductTags();
-  // console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+  await seedExchanges();
+  console.log("\n----- EXCHANGES SEEDED -----\n");
 
   process.exit(0);
 };
 
 seedAll();
-=======
-const seedUsers = require('./user-seeds');
-const seedCurrencies = require('./currency-seeds');
-const seedExchanges = require('./exchange-seeds');
-const seedPosts = require('./post-seeds');
-
-const sequelize = require('../config/connection');
-
-
-const seedAll = async () => {
-    await sequelize.sync({
-        force: true
-    });
-    console.log('\n----- DATABASE SYNCED -----\n');
-
-    await seedUsers();
-    console.log('\n----- USERS SEEDED -----\n');
-
-    await seedCurrencies();
-    console.log('\n----- CURRENCIES SEEDED -----\n');
-
-    await seedPosts();
-    console.log('\n----- POSTS SEEDED -----\n');
-
-    await seedExchanges();
-    console.log('\n----- EXCHANGES SEEDED -----\n');
-
-    process.exit(0);
-};
-
-seedAll();
->>>>>>> fe251787c9948de32ec5be36fcc931204cf3ae7e
