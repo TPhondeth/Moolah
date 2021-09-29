a6ad3109f7c7e2fd4253e49930841fefa9630913
Tony Phondeth <phondeth@gmail.com>
2021-09-29 14:44:03 -0400

    created User-Currency

--- models/index.js
+++ models/index.js
@@ -3,16 +3,27 @@ const Currency = require('./Currency');
 const Post = require('./Post');
 const User = require('./User');
 const Exchange = require('./Exchange');
+const UserCurrency = require('./User-Currency');
 
 
 // Create associations
-User.hasMany(Currency, {
-    foreignKey: 'user_id'
-});
+// User.hasMany(Currency, {
+//     foreignKey: 'user_id'
+// });
 
-Currency.belongsTo(User, {
+// Currency.belongsTo(User, {
+//     foreignKey: 'user_id'
+// });
+
+User.belongsToMany(Currency, {
+    through: UserCurrency,
     foreignKey: 'user_id'
-});
+})
+
+Currency.belongsToMany(User, {
+    through: UserCurrency,
+    foreignKey: 'currency_id'
+})
 
 Post.belongsTo(User, {
     foreignKey: 'user_id'
@@ -22,9 +33,11 @@ User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
+
 module.exports = {
     User,
     Post,
     Currency,
-    Exchange
+    Exchange,
+    UserCurrency
 };
