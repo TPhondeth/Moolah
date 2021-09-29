de68830fbcc53dbe1ebbc3c70c07b340cc255f99
Sam Thabet <sallam.thabet@gmail.com>
2021-09-28 20:36:47 -0400

    modified code for handlebars front end

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,30 +1,48 @@
 // Dependencies
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const {
-    Post,
-    User,
-    Currency,
-    Exchange
-} = require('../models');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Currency, Exchange } = require("../models");
 
-router.get('/', (req, res) => {
-    res.render('homepage', {
-        loggedIn: req.session.loggedIn
+router.get("/", (req, res) => {
+  console.log("======================");
+  Post.findAll({
+    // attributes: ["id", "post_text", "title", "user_id"],
+    // include: [
+    //   {
+    //     model: Currency,
+    //     attributes: ["id", "currency", "currency_name", "price"],
+    //     include: {
+    //       model: User,
+    //       attributes: ["username"],
+    //     },
+    //   },
+    //   {
+    //     model: User,
+    //     attributes: ["username"],
+    //   },
+    // ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+      res.render("homepage", {
+        posts,
+        //loggedIn: req.session.loggedIn,
+      });
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
-})
-
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-
-    res.render('login');
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
+// router.get("/login", (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.redirect("/");
+//     return;
+//   }
+
+//   res.render("login");
+// });
 
 module.exports = router;
