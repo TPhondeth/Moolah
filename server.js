3c48dd3e3402069672b73cdd16f7d4fb42c86bf5
Tony Phondeth <phondeth@gmail.com>
2021-09-29 12:40:39 -0400

    added session

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 // Import sequelize connection
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
