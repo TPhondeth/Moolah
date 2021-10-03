650aea731931e34d2b7501e15eaec6abd9bafddb
Sam Thabet <sallam.thabet@gmail.com>
2021-10-03 02:18:23 -0400

    adding a holding Handlebars fixed some connectivity within login and portfolio added an h4 to stylesheet

--- public/javascript/add-currency.js
+++ public/javascript/add-currency.js
@@ -1,26 +1,31 @@
 async function newFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const currency = document.querySelector('input[name="currency"]').value;
-    const currency_name = document.querySelector('input[name="currency_name"]').value;
+  const currency = document.querySelector('input[name="currency"]').value;
+  const currency_name = document.querySelector(
+    'input[name="currency_name"]'
+  ).value;
+  const curr_ownd = document.querySelector('input[name="curr_ownd"]').value;
 
-    const response = await fetch(`/api/currencies`, {
-        method: 'POST',
-        body: JSON.stringify({
-            currency,
-            currency_name,
-            price
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+  const response = await fetch(`/api/currencies`, {
+    method: "POST",
+    body: JSON.stringify({
+      currency,
+      currency_name,
+      curr_ownd,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if (response.ok) {
-        document.location.replace('/portfolio');
-    } else {
-        alert(response.statusText);
-    }
+  if (response.ok) {
+    document.location.replace("/portfolio");
+  } else {
+    alert(response.statusText);
+  }
 }
 
-document.querySelector('.new-currency-form').addEventListener('submit', newFormHandler);
+document
+  .querySelector(".new-currency-form")
+  .addEventListener("submit", newFormHandler);
